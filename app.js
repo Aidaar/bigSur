fe2741b757452610df9e1d5a9f7fb2a43d9c4382
Aidaar <aidar21mamatov@gmail.com>
2023-12-14 22:40:32 +0600

    add to slider page

--- app.js
+++ app.js
@@ -1,53 +1,145 @@
-let items = document.querySelectorAll('.slider .list .item');
-let next = document.getElementById('next');
-let prev = document.getElementById('prev');
-let thumbnails = document.querySelectorAll('.thumbnail .item');
+function PersonSlider(gridSelector, nameSelector, copySelector, peopleSelector, slidersSelector, sliderIndexes, controlPrevSelector, controlNextSelector) {
+	this.grid = document.querySelector(gridSelector);
+	this.sliderNames = document.querySelectorAll(nameSelector);
+	this.copy = Array.from(this.grid.querySelectorAll(copySelector));
+	this.people = Array.from(this.grid.querySelectorAll(peopleSelector));
+	this.sliders = Array.from(document.querySelectorAll(slidersSelector));
+	this.sliderIndexes = Array.from(document.querySelectorAll(sliderIndexes));
+	this.controlPrev = document.querySelector(controlPrevSelector);
+	this.controlNext = document.querySelector(controlNextSelector);
+	this.currentIndex = 0;
 
-// config param
-let countItem = items.length;
-let itemActive = 0;
-// event next click
-next.onclick = function(){
-    itemActive = itemActive + 1;
-    if(itemActive >= countItem){
-        itemActive = 0;
-    }
-    showSlider();
+	this.init = function () {
+			this.setListeners();
+	};
+
+	this.setListeners = function () {
+			var self = this;
+			this.controlPrev.addEventListener('click', function () {
+					self.handlePrev();
+			});
+			this.controlNext.addEventListener('click', function () {
+					self.handleNext();
+			});
+	};
+
+	this.handlePrev = function () {
+			this.currentIndex--;
+			if (this.currentIndex < 0) {
+					this.currentIndex = this.people.length - 1;
+			}
+			this.setActiveClass();
+	};
+
+	this.handleNext = function () {
+			this.currentIndex++;
+			if (this.currentIndex > this.people.length - 1) {
+					this.currentIndex = 0;
+			}
+			this.setActiveClass();
+	};
+
+	this.setActiveClass = function () {
+			var self = this;
+			this.sliderNames.forEach(function (name, i) {
+					name.classList.toggle('active', i === self.currentIndex);
+			});
+			this.copy.forEach(function (pTag, i) {
+					pTag.classList.toggle('active', i === self.currentIndex);
+			});
+			this.people.forEach(function (person, i) {
+					person.classList.toggle('active', i === self.currentIndex);
+			});
+			this.sliders.forEach(function (slide, i) {
+					slide.classList.toggle('active', i === self.currentIndex);
+			});
+			this.sliderIndexes.forEach(function (slideIndex, i) {
+					slideIndex.classList.toggle('active', i === self.currentIndex);
+			});
+	};
 }
-//event prev click
-prev.onclick = function(){
-    itemActive = itemActive - 1;
-    if(itemActive < 0){
-        itemActive = countItem - 1;
-    }
-    showSlider();
+
+function ImageSlider(parent, slidesSelector, currentImageNumElSelector, totalImageNumElSelector, controlPrevSelector, controlNextSelector) {
+	this.slides = Array.from(parent.querySelectorAll(slidesSelector));
+	this.currentImageNumEl = parent.querySelector(currentImageNumElSelector);
+	this.totalImageNumEl = parent.querySelector(totalImageNumElSelector);
+	this.controlPrev = parent.querySelector(controlPrevSelector);
+	this.controlNext = parent.querySelector(controlNextSelector);
+	this.currentIndex = 0;
+	this.totalImageNum = this.slides.length;
+
+	this.init = function () {
+			this.setTotalPageNum();
+			this.setListeners();
+	};
+
+	this.setListeners = function () {
+			var self = this;
+			this.controlPrev.addEventListener('click', function () {
+					self.handlePrev();
+			});
+			this.controlNext.addEventListener('click', function () {
+					self.handleNext();
+			});
+	};
+
+	this.handlePrev = function () {
+			this.currentIndex--;
+			if (this.currentIndex < 0) {
+					this.currentIndex = this.slides.length - 1;
+			}
+			this.setActiveClass();
+			this.setCurrentImageNum();
+	};
+
+	this.handleNext = function () {
+			this.currentIndex++;
+			if (this.currentIndex > this.slides.length - 1) {
+					this.currentIndex = 0;
+			}
+			this.setActiveClass();
+			this.setCurrentImageNum();
+	};
+
+	this.setActiveClass = function () {
+			var self = this;
+			this.slides.forEach(function (slide, i) {
+					slide.classList.toggle('active', i === self.currentIndex);
+			});
+	};
+
+	this.setCurrentImageNum = function () {
+			this.currentImageNumEl.innerHTML = this.currentIndex + 1;
+	};
+
+	this.setTotalPageNum = function () {
+			this.totalImageNumEl.innerHTML = this.totalImageNum;
+	};
 }
-// auto run slider
-let refreshInterval = setInterval(() => {
-    next.click();
-}, 5000)
-function showSlider(){
-    // remove item active old
-    let itemActiveOld = document.querySelector('.slider .list .item.active');
-    let thumbnailActiveOld = document.querySelector('.thumbnail .item.active');
-    itemActiveOld.classList.remove('active');
-    thumbnailActiveOld.classList.remove('active');
 
-    // active new item
-    items[itemActive].classList.add('active');
-    thumbnails[itemActive].classList.add('active');
+var personSliderInstance = new PersonSlider(
+	'.portfolio-slider__left-cont__grid',
+	'.portfolio-slider__left-cont__name-cont__name span',
+	'.portfolio-slider__left-cont__grid__copy p',
+	'.js-person',
+	'.portfolio-slider__image-slider-cont__slider',
+	'.portfolio-slider__image-slider-cont__controls-cont__index',
+	'.portfolio-slider__left-cont__name-cont__controls__prev',
+	'.portfolio-slider__left-cont__name-cont__controls__next'
+);
 
-    // clear auto time run slider
-    clearInterval(refreshInterval);
-    refreshInterval = setInterval(() => {
-        next.click();
-    }, 5000)
-}
+personSliderInstance.init();
 
-// click thumbnail
-thumbnails.forEach((thumbnail, index) => {
-    thumbnail.addEventListener('click', () => {
-        itemActive = index;
-        showSlider();
-    })
-})
+var imageSliders = document.querySelectorAll('.portfolio-slider__image-slider-cont__slider');
+
+Array.from(imageSliders).forEach(function (slider) {
+	slider = new ImageSlider(
+			slider,
+			'.js-slide',
+			'.js-current-image-num',
+			'.js-total-image-num',
+			'.portfolio-slider__image-slider-cont__slider__controls-cont__prev',
+			'.portfolio-slider__image-slider-cont__slider__controls-cont__next'
+	);
+	slider.init();
+});
